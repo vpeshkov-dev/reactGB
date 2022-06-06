c806fe80ff0aeb20f28e91f8746f9b4c5f8ac106
Vladimir Vladimir <peshkovdev@gmail.com>
2022-06-06 17:05:14 +0300

    устнаовил redux devtools

--- chat/src/store/create-store.js
+++ chat/src/store/create-store.js
@@ -1,6 +1,11 @@
+/* eslint-disable */
+
 import { createStore, combineReducers } from 'redux'
 import profileReducer from './profile/reducer'
 
-const store = createStore(combineReducers({ profile: profileReducer }))
+const store = createStore(
+    combineReducers({ profile: profileReducer }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
